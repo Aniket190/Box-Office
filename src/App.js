3b61fab90358e81c8946efc8c4135e88910c68d3
Aniket <aniketkumar190@gmail.com>
2020-12-01 10:33:27 +0530

    addded router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>Hello</div>
+    <Switch>
+      <Route exact path="/">This is Home Page</Route>
+      <Route exact path="/stared">This is starred</Route>
+      <Route>404!</Route>
+    </Switch>
   );
 }
 
